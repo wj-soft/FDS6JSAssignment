061f67fb63ed6c7eae244465005921ae52a469a8
wj-soft <wooju_lee@daum.net>
2017-10-13 10:41:00 +0900

    add solve

--- wooju_lee/20171012/Q10.js
+++ wooju_lee/20171012/Q10.js
@@ -2,34 +2,18 @@
 
 // palindrome(팰린드롬/회문)은 왼쪽에서 오른쪽으로 읽은 다음, 오른쪽부터 왼쪽으로 다시 읽어도 똑같은 형태와 의미를 유지하는 문장이나 단어를 지칭한다. 인자로 전달한 문자열이 palindrome인지 검사하여 Boolean값을 반환하는 함수를 완성하라. 단, 반드시 1자 이상의 문자열을 인자로 전달한다.
 
-// function checkPalindrom(str) {
-//   res = '';
-//   if (str % 2 === 1) {
-//     for (i = 0; i < str.length; i++) {
-//       res += str[i] === str[str.length - 1 - i]
-//     };
-//     return res;
-//   }
-//   console.log(res)
-
-// }
-
-// function checkPalindrom(str) {
-//   strArr = str.split('');
-//   strReverse = str.split('').reverse();
-//   return strArr === strReverse;
-// }
-
 function checkPalindrom(str) {
-  origin = '';
-  reverse = '';
-  for (i = 0; i < str.length; i++) {
-    origin += str[i];
-  };
-  for (j = str.length - 1; j >= 0; j--) {
-    reverse += str[j];
-  };
-  return origin === reverse;
+  return str === str.split('').reverse().join('');
+
+  // origin = '';
+  // reverse = '';
+  // for (i = 0; i < str.length; i++) {
+  //   origin += str[i];
+  // };
+  // for (j = str.length - 1; j >= 0; j--) {
+  //   reverse += str[j];
+  // };
+  // return origin === reverse;
 
 }
 
--- wooju_lee/20171012/fortest.js
+++ wooju_lee/20171012/fortest.js
@@ -2,3 +2,5 @@
  for (j = a.length - 1; j >= 0; j--) {
    console.log(a[j]);
  };
+
+ console.log(Math.sqrt(10)); // 3
